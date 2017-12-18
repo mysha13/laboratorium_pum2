4adda54b0eb6a122ed4426344b72b4f59f384da3
jpienko <j.pienko@onet.pl>
2017-12-18 11:36:43 +0100

    setOnClickListenear
    add

--- app/src/main/java/com/example/student/aplikacjapogodowa/MainActivity.java
+++ app/src/main/java/com/example/student/aplikacjapogodowa/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.student.aplikacjapogodowa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Miejsce_but=findViewById(R.id.miejsce_button);
+        Button Lista_but=findViewById(R.id.lista_button);
+        final EditText edit =findViewById(R.id.pole_edycji);
+        final TextView Miasto  =findViewById(R.id.miasto);
+
+        Miejsce_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text=edit.getText().toString();
+                Miasto.setText(text);
+
+            }
+        });
+
+        Lista_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
     }
+
+
+
 }
